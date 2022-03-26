7b6fb564ded3fb108f2da14a6951e6285a7c4b38
MathsCode <nthaxjm@126.com>
2022-03-26 22:15:59 +0800

    Test3

--- test.py
+++ test.py
@@ -2,9 +2,10 @@
 Description: Machine Learning Test
 Author: Xu Jiaming
 Date: 2022-03-26 21:53:03
-LastEditTime: 2022-03-26 22:07:36
+LastEditTime: 2022-03-26 22:15:43
 LastEditors:  
 FilePath: test.py
 '''
+print("hello world!")
 print("hello world!")
 print("hello world!")
